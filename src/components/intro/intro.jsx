66229d317c5dac8d5edfc5029b43b677c7827d7a
pangpuncake <pangpuncake@gmail.com>
2020-12-08 01:58:09 +0800

    Change font sizes

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -4,11 +4,11 @@ import './intro.css'
 
 const Intro = () => (
     <div className="intro-container">
-        <h2 className="intro-h2">Welcome to EatsBetween!</h2>
+        <h1 className="intro-h1">Welcome to EatsBetween!</h1>
 
-        <p className="intro-p">
+        <h3 className="intro-p">
             Find a convenient location between your friends to have a great time together!
-        </p>
+        </h3>
     </div>
 )
 
